337d0e37e2f7e86bed852d8cf89871d06b629b6a
정재현 <chwogus0303@naver.com>
2024-10-24 17:09:32 +0900

    optimize(springboot-app): import 최적화

--- springboot-app/src/main/java/com/inha/springbootapp/domain/ping/HelloController.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/ping/HelloController.java
@@ -1,10 +1,9 @@
 package com.inha.springbootapp.domain.ping;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 // 컨트롤러에서 CORS를 개별적으로 설정하는 방법
 // @CrossOrigin(origins = "http://localhost:3001") // React 애플리케이션의 URL
