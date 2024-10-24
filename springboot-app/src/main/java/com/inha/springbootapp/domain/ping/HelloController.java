e0441328ca009f08f5e2bf4305445e1de6d32f8a
정재현 <chwogus0303@naver.com>
2024-10-24 16:51:27 +0900

    refactor(springboot-app): 파일 경로 수정

--- springboot-app/src/main/java/com/inha/springbootapp/domain/ping/HelloController.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/ping/HelloController.java
@@ -1,4 +1,4 @@
-package com.inha.springbootapp;
+package com.inha.springbootapp.domain.ping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
