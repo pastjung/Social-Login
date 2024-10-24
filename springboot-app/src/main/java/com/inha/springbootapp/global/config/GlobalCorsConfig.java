11811a4c323d928ec9ce667b0215c7cbf740d85e
정재현 <chwogus0303@naver.com>
2024-10-24 16:51:27 +0900

    refactor(springboot-app): 파일 경로 수정

--- springboot-app/src/main/java/com/inha/springbootapp/global/config/GlobalCorsConfig.java
+++ springboot-app/src/main/java/com/inha/springbootapp/global/config/GlobalCorsConfig.java
@@ -1,15 +1,12 @@
-package com.inha.springbootapp;
+package com.inha.springbootapp.global.config;
 
-import org.springframework.context.annotation.Bean;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @Configuration
 public class GlobalCorsConfig implements WebMvcConfigurer {
-
     Dotenv dotenv = Dotenv.load();
     private final String reactURL = "http://localhost:" + dotenv.get("REACT_HOST_PORT");
 
